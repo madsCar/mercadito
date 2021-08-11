53ee3a8b1453a773fcf8867d333dd94f5fd296ba
Alexis17mx <alexisgalvan94@gmail.com.mx>
2021-08-11 14:20:45 -0500

    empleados

--- MercaditoRecargado/Models/Empleado.cs
+++ MercaditoRecargado/Models/Empleado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,10 +11,13 @@ namespace MercaditoRecargado.Models
         public int EmpleadoID { get; set; }
         public string CodigoEmpleado { get; set; }
         public string puesto { get; set; }
+        [Display(Name = "Fecha de ingreso")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> fechaIngreso { get; set; }
         public string salario { get; set; }
         public int Estatus { get; set; }
-        public int EmpleadoDatos { get; set; }
+        public int PersonaID { get; set; }
         public string EmlpleadoUser { get; set; }
 
         //public virtual AspNetUser AspNetUser { get; set; }
